b8abfca265e18b563d589fc797d02ec6e75374ff
ashverya5293 <57065959+ashverya5293@users.noreply.github.com>
2019-11-06 15:06:52 +0530

    Add files via upload

--- Q6_MT2018502.s
+++ Q6_MT2018502.s
@@ -2,23 +2,26 @@
 	EXPORT __main
 	ENTRY
 __main  FUNCTION
-
-		MOV R0, #6 ;Loading content of a in register r0  
-		MOV R1, #15;Loading content of b in register r1
-
+; GCD program  R3 shows the final value of GCD
+		MOV R0, #6   ;Loading content of a in register r0  
+		MOV R1, #15  ;Loading content of b in register r1
+loop
 loop2 	CMP R0, R1 ; Comparing two values
-		BNE loop ; Branch if R0 not equal to R1
+	BEQ equal ; Branch if R0 not equal to R1
+  	BGT grt ; Branch if a is greater than b
+	B less
+
+grt	SUB R0, R1 ; Execute this line if b>a
+	MOV R3,R0; R3 stores the result of GCD
+	B loop
+
+less SUB R1, R0; Execute this line if a>b
+	 MOV R3,R1; R3 stores the result of GCD
+	 
+	B loop
+	B loop2
+
+equal
 stop    B stop ; stop program
-
-loop  	BGT grt ; Branch if a is greater than b
-
-		SUB R1, R1, R0 ; Execute this line if b>a
-		B loop1
-grt 	SUB R0, R0, R1 ; Execute this line if a>b
-
-
-loop1	CMP R0, R1
-		BNE loop2
- 
 	ENDFUNC
 	END
